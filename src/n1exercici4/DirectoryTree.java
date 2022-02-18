148c0eff3d4a714bb8117f8524dc358bafcb9a8c
Quique Sánchez Suárez <kikeebcn@gmail.com>
2022-02-18 09:37:17 +0100

    n1 - exercici4 - cambios en menú.

--- src/n1exercici4/DirectoryTree.java
+++ src/n1exercici4/DirectoryTree.java
@@ -11,6 +11,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DirectoryTree {
@@ -21,63 +22,79 @@ public class DirectoryTree {
 		String pathToFile;
 		
 		do {
+			
 			System.out.println("\nEscoge una opción:"
 					+ "\n1- Buscar ruta de directorio y escribir archivo .TXT con listado de archivos"
 					+ "\n2- Buscar archivo .TXT y leer contenido por consola"
 					+ "\n3- Salir");
-			
-			menu = sc.nextInt();
-			sc.skip("\n");
-			
-			switch(menu) {
-			
-				case 1: 
-					
-					System.out.println("Introduce ruta de directorio:");
-					File dir = new File(sc.nextLine());
-					
-					try {
-						
-						System.out.println("Indica el nombre y ruta del archivo a guardar:");
-						outTxt = new BufferedWriter(new FileWriter(sc.nextLine()));
-						getListFiles(dir.listFiles());
-						System.out.println("Archivo guardado con éxito.\n");
-						outTxt.close();
-						
-					} catch (NullPointerException e) {
-						
-						System.out.println("Directorio no encontrado.");
-						
-					} catch (IOException e) {
-						
-						System.out.println("Ruta incorrecta.\n");
-						
-					}
-					
-					break;
-					
-				case 2:	
+			try {
 				
-					System.out.println("Introduce ruta del archivo a leer:");
-					pathToFile = sc.nextLine();
-					readFile(pathToFile);
+				menu = sc.nextInt();
+				sc.skip("\n");
+				
+				switch(menu) {
+				
+					case 1: 
+						
+						System.out.println("Introduce ruta de directorio:");
+						File dir = new File(sc.nextLine()); // Lee la ruta del directorio que le pasamos por consola.
+						
+						try {
+							
+							System.out.println("Indica el nombre y ruta del archivo a guardar:");
+							outTxt = new BufferedWriter(new FileWriter(sc.nextLine()));
+							getListFiles(dir.listFiles()); // Devuelve array de Files[] de lo que hay dentro del directorio introducido. Mandamos a método getListFiles.
+							System.out.println("Archivo guardado con éxito.\n");
+							outTxt.close();
+							
+						} catch (NullPointerException e) {
+							
+							System.out.println("Directorio no encontrado.");
+							
+						} catch (IOException e) {
+							
+							System.out.println("Ruta incorrecta.\n");
+							
+						}
+						
+						break;
+						
+					case 2:	
 					
-					break;
-			
+						System.out.println("Introduce ruta del archivo a leer:");
+						pathToFile = sc.nextLine();
+						readFile(pathToFile);  // Método para leer archivos. 
+						System.out.println("Archivo leído con éxito.\n");
+						
+						break;
+						
+					case 3:
+						
+						System.out.println("\nPrograma finalizado.");
+						
+						break;
+				
 			}
+			
+			} catch (InputMismatchException e) {
+				
+				System.out.println("Opción incorrecta: debes introducir un número.");
+				sc.nextLine();
+			} 
+			
 		
-		} while (menu != 3);
+		} while (menu < 1 || menu > 3);
 		
 	}	
 	
 	 public static void getListFiles(File[] dir) throws NullPointerException, IOException {
 		 
-		 DateFormat milliToDate = new SimpleDateFormat("dd MMM yyyy HH:mm:ss:SSS Z");
-		 Arrays.sort(dir);
+		 DateFormat milliToDate = new SimpleDateFormat("dd MMM yyyy HH:mm:ss:SSS Z"); // Indicamos el formato de la fecha que vamos a representar con los milisegundos. 
+		 Arrays.sort(dir); // Ordenamos el array de Files[] por orden alfabético.
 		 
-		 for(File f : dir) {
+		 for(File f : dir) { // Navegamos por el array.
 			 
-			 Date milli = new Date(f.lastModified());
+			 Date milli = new Date(f.lastModified()); // Pasamos de milisegundos a formato Date.
 			 
 			 try {
 				 
@@ -85,7 +102,7 @@ public class DirectoryTree {
 					 
 					 System.out.println(f.getAbsolutePath() + " (D)");
 					 outTxt.write(f.getAbsolutePath() + " (D) - Última modificación: " + milliToDate.format(milli)  + "\n");
-					 getListFiles(f.listFiles());
+					 getListFiles(f.listFiles()); // Hacemos la función recursiva para que "navegue" entre el resto de directorios.
 					 
 				 } else {
 					 
@@ -106,7 +123,7 @@ public class DirectoryTree {
 	 public static void readFile(String pathToFile) throws IOException {
 		 
 		 BufferedReader out = null;
-		 String strCurrentLine;
+		 String stringLine;
 		 
 		 try {
 			 
@@ -114,9 +131,9 @@ public class DirectoryTree {
 			
 			System.out.println("\nLeyendo archivo...\n");
 			
-			while((strCurrentLine = out.readLine()) != null) {
+			while((stringLine = out.readLine()) != null) {
 				
-				System.out.println(strCurrentLine);
+				System.out.println(stringLine);
 				
 			}
 			
